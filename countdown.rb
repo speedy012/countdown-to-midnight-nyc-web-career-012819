588e26dbf3adc5992c96d857de4feeccb3795e65
Herman <hrivas5726@aol.com>
2019-01-16 04:29:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,14 @@
 #write your code here
 
 def countdown(some_int)
-  while x > 0  
-   "#{x} SECOND(S)!"
-  x-=1
+  while some_int > 0  
+   "#{some_int} SECOND(S)!"
+  x -=1
   end
  "HAPPY NEW YEAR!"
 end
+
+
 
 
 
