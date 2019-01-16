a31695ff2635acab0efa9809876c9e42caaf3baa
Herman <hrivas5726@aol.com>
2019-01-16 04:29:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,13 @@ def countdown(some_int)
  "HAPPY NEW YEAR!"
 end
 
-
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
 
 
